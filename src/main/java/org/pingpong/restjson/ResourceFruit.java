21d597656c4e4e26b6e0a240e44c6136efd71626
dfleta <gelpiorama@gmail.com>
2021-05-17 19:38:19 +0200

    feat(ResourceFruit): Implementar negociacion de contenido TEXT / JSON

--- src/main/java/org/pingpong/restjson/ResourceFruit.java
+++ src/main/java/org/pingpong/restjson/ResourceFruit.java
@@ -22,18 +22,30 @@ public class ResourceFruit {
         // CDI
     }
 
+    /**
+     * Los metodos hello() y list()
+     * negocian con el content-type del header
+     * de la peticion http
+     * hello -> content-type text
+     * list() -> content application/json
+     */
+    
     @GET
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
+    // $ curl -w "\n" http://localhost:8080/fruits/ 
+    // -H "Content-Type: application/x-www-form-urlencoded"
     public String hello() {
         return "Colmados Farmer Rick";
     }
 
     @GET
-    @Path("/list")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     // no es necesario Produces ya que por defecto
     // resteasy jackson desactiva la negociación
     // y sirve MediaType.APPLICATION_JSON
+    // $ curl -w "\n" http://localhost:8080/fruits/ -H "Content-Type: application/json"
     public Set<Fruit> list() {
         return service.list();
     }
@@ -53,7 +65,7 @@ public class ResourceFruit {
     @Produces(MediaType.APPLICATION_JSON)
     // $ curl -d '{"name":"Banana", "description":"Brings a Gorilla too"}'
     // -H "Content-Type: application/json" -X DELETE http://localhost:8080/fruits   
-    public Set<Fruit> delete(Fruit fruit) {
+    public Set<Fruit> delete(@Valid Fruit fruit) {
         service.remove(fruit.name);
         return list();
     }
--- src/test/java/org/pingpong/restjson/FruitResourceTest.java
+++ src/test/java/org/pingpong/restjson/FruitResourceTest.java
@@ -2,12 +2,18 @@ package org.pingpong.restjson;
 
 import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 
@@ -21,7 +27,9 @@ public class FruitResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+        // content-type de la peticion es TEXT para endpoint hello
         given()
+            .contentType(ContentType.TEXT)
             .when().get("/fruits")
             .then()
                 .statusCode(200)
@@ -29,10 +37,25 @@ public class FruitResourceTest {
     }
 
     @Test
+    public void testListEndpoint() {
+        // list() endpoint devuelve lista de maps [{}, {}]
+        List<Map<String, Object>> products = 
+            given()
+                .contentType(ContentType.JSON)
+                .when().get("/fruits")
+                .as(new TypeRef<List<Map<String, Object>>>() {});
+
+        Assertions.assertThat(products).hasSize(2);
+        Assertions.assertThat(products.get(0)).containsValue("Apple");
+        Assertions.assertThat(products.get(1)).containsEntry("description", "Tropical fruit");
+    }
+
+    @Test
     @TestTransaction
     public void testList() {
         given()
-            .when().get("/fruits/list")
+            .contentType(ContentType.JSON)
+            .when().get("/fruits/")
             .then()
                 .statusCode(200)
                 .body("$.size()", is(2),
